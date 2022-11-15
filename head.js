0c9333e18925c9c2551745343577560764210db0
Jimmy <jimmyzhang1@hotmail.com>
2022-11-14 17:22:09 -0800

    Head function

---
+++ head.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(`✅✅✅ Assertion Passed. ${actual} === ${expected}`);
+  }
+
+  console.log(`🛑🛑🛑 Assertion Failed. ${actual} !== ${expected}`);
+};
+
+const head = function(array) {
+  return array[0];
+};
+
+console.log(head([]));
+// assertEqual(head([5, 6, 7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
