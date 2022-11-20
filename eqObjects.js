e74e085113e2af805fafd32caf72252293118be9
Jimmy <jimmyzhang1@hotmail.com>
2022-11-19 16:37:43 -0800

    Added recursive feature that checks for objects within the object

--- eqObjects.js
+++ eqObjects.js
@@ -24,7 +24,10 @@ const eqArrays = (arr1, arr2) => {
 // Our eqObjects function:
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
+
 const eqObjects = function(object1, object2) {
+  // console.log('object1:', object1);
+  // console.log('object2:', object2);
 
   // Are the object lengths equal?
   if (Object.keys(object1).length !== Object.keys(object2).length) {
@@ -34,35 +37,35 @@ const eqObjects = function(object1, object2) {
   // If that passes, we can test each key value pair
   for (let key1 in object1) {
 
-    for (let key2 in object2) {
-      let value1 = object1[key1];
-      let value2 = object2[key2];
-      // console.log("key1:", key1);
-      // console.log("value1:", value1,);
-      // console.log("key2:", key2);
-      // console.log("value2:", value2,);
+    const value1 = object1[key1];
+    const value2 = object2[key1];
+    // console.log('value1:', value1);
+    // console.log('value2:', value2);
 
-      // if keys are the same, and the key values are the same
-      // also, if the key is an array, we use eqArray
-      // console.log(`Key 1 of ${object1}: ${key1}, Key 2 of ${object2}: ${key2}`);
-      if (key1 === key2) {
-        // If the key is an array, we eqArray
-        if (Array.isArray(value1) && Array.isArray(value2)) {
-          // console.log(eqArrays(value1, value2));
-          if (!eqArrays(value1, value2)) {
-            return false;
-          }
-        } else if (value1 !== value2) {
-          return false;
-        }
-      }
+    // If the key is an array, we use eqArray
+    if (Array.isArray(value1) && Array.isArray(value2) && !eqArrays(value1, value2)) {
+      // console.log(eqArrays(value1, value2));
+      // console.log('line 21');
+      return false;
+
+      // If the keys are both objects (confirmed to not be arrays)
+    } else if (typeof value1 === 'object' && typeof value2 === 'object' && !eqObjects(value1, value2)) {
+      // console.log(eqObjects(value1, value2))
+      // console.log('line 28');
+      return false;
+
+    } else if (value1 !== value2) {
+      // console.log(value1 === value2);
+      return false;
+
     }
+
+
+
   }
 
   return true;
 };
-
-
 
 //Tests
 // const ab = { a: "1", b: "2" };
@@ -81,5 +84,14 @@ console.log(eqObjects(cd, dc));
 // => true
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
-console.log(eqObjects(cd, cd2))
+console.log(eqObjects(cd, cd2));
 // => false
+
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })); //false
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 })); // false
+console.log(eqObjects({ a: { y: 0, z: { z: 1, b: 3 }, b: 2 } }, { a: 1, b: 2 })); // false
+console.log(eqObjects({ a: { y: 0, z: { z: 1, b: 3 }, b: 2 } }, { a: { y: 0, z: { z: 1, b: 3 }, b: 2 } })); //true
+console.log(eqObjects({ a: { y: 0, z: { z: 1, b: 3 }, b: 2 } }, { a: { y: 0, z: { z: 1, b: 6 }, b: 2 } })); // false
+console.log(eqObjects({ a: { y: 0, z: { z: 1 }, b: 2 } }, { a: { y: 0, z: { z: 1 }, b: 15 } })); // false
+
+
