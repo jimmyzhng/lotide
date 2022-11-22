3e3ff335e3fd7cd92cc6b3da56e9a544cd472d36
Jimmy <jimmyzhang1@hotmail.com>
2022-11-21 16:54:54 -0800

    added mocha/chai testing, removed old testing

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,13 +1,22 @@
 const middle = require('../middle');
-const eqArrays = require('../eqArrays');
-const assertArraysEqual = require('../assertArraysEqual');
-
+const assert = require('chai').assert;
 
 // TEST CODE
 
 const words = ["hello", "world", "lighthouse"];
 const wordsEven = ["hello", "world", "lighthouse", "labs"];
-assertArraysEqual(middle([]), []);
-assertArraysEqual(middle(words), "world");
-assertArraysEqual(middle(wordsEven), ["world", "lighthouse"]);
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+
+describe('#middle', () => {
+  it('should return empty array if given empty array', () => {
+    assert.deepEqual(middle([]), []);
+  });
+
+  it('should return element of the middle index (odd length)', () => {
+    assert.deepEqual(middle(words), "world");
+  });
+
+  it('should return two middle-most elements (even length)', () => {
+    assert.deepEqual(middle(wordsEven), ["world", "lighthouse"]);
+  });
+
+});
